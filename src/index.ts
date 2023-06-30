d9f5e7fcd4edfe2b1e2ec0c467aa79b53aca978a
Emmanuel SMITH <emmanuel.smith@jagfx.fr>
2023-06-30 23:06:25 +0200

    :sparkles: add translator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // --- application
-export { logger } from './application/logger';
+export * as logger from './application/logger';
+export * as toast from './application/toast';
+export * as translator from './application/translator';
 
 // --- isNil
 export { utils } from './isNil';
